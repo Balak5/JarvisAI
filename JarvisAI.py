64976dc14ceec40d6d1b4f0792248d3b2ebe31ef
Beamlak <beamlakalemseged@gmail.com>
2022-02-18 13:05:56 +0300

    greetings done

--- JarvisAI.py
+++ JarvisAI.py
@@ -1,4 +1,6 @@
+from calendar import month
 import pyttsx
+import datetime
 
 engine = pyttsx.init()
 
@@ -14,11 +16,75 @@ def setVoice(value):
     elif value == 2:
         engine.setProperty('voice', voices[0].id)
 
-value = int(input('Welcome, Press 1 for male voice\n Press 2 for female voice.\n'))
+def time():
+    time = datetime.datetime.now().strftime("%I:%M:%S") # hour minute second
+    speak('the time is -')
+    speak(time)
 
-setVoice(value)
+def date():
+    # make the 
+    year = datetime.datetime.now().year
+    month  = datetime.datetime.now().month
+    day = datetime.datetime.now().day
 
-text = input('Hello there, enter a command!\n')
+    speak('the current date is ')
+    speak(getMonth(month))
+    speak(day)
+    speak(year)
 
-speak(text)
+def getMonth(monthNum):
+    if monthNum == 1:
+        return 'January'
+    elif monthNum == 2:
+        return 'February'
+    elif monthNum == 3:
+        return 'March'
+    elif monthNum == 4:
+        return 'April'
+    elif monthNum == 5:
+        return 'May'
+    elif monthNum == 6:
+        return 'June'
+    elif monthNum == 7:
+        return 'July'
+    elif monthNum == 8:
+        return 'August'
+    elif monthNum == 9:
+        return 'Spetember'
+    elif monthNum == 10:
+        return 'October'
+    elif monthNum == 11:
+        return 'November'
+    elif monthNum == 12:
+        return 'December'
 
+def greeting():
+    hour = datetime.datetime.now().hour
+    if hour >= 6 and hour < 12:
+        speak('Good Morning sir')
+    elif hour >= 12 and hour < 17:
+        speak('Good Afternoon sir')
+    elif hour > 17 and hour < 19:
+        speak('Good Evening sir')
+    else:
+        speak('How is your night sir')
+
+def wishme():
+    setVoice(1)
+    speak('Welcome back sir')
+    time()
+    date()
+    greeting()
+    speak('Jarvis at your service, your command is my wish')
+
+# while True:
+
+#     value = int(input('Welcome, Press 1 for male voice\n Press 2 for female voice.\n'))
+
+#     setVoice(value)
+
+#     text = input('Hello there, enter a command!\n')
+
+#     speak(text)
+
+wishme()
